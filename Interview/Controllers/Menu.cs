3d526deca8b3cac8077af4143256fc85f848d18a
Frank <franklp5010606@gmail.com>
2024-06-22 13:39:41 +0800

    202406221338-新增能搜尋到菜單單名

--- Interview/Controllers/Menu.cs
+++ Interview/Controllers/Menu.cs
@@ -2,6 +2,7 @@
 using Interview.Data;
 using Interview.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
 
 namespace Interview.Controllers
 {
@@ -13,9 +14,16 @@ namespace Interview.Controllers
         {
             _context = context;
         }
-        public async Task<IActionResult> Index()
+        public async Task<IActionResult> Index(string searchString)
         {
-            return View( await _context.Dishes.ToListAsync());
+            var dishes = from d in _context.Dishes
+                       select d;
+            if(!string.IsNullOrEmpty(searchString))
+            {
+                dishes = dishes.Where(d => d.Name.Contains(searchString));
+                return View(await dishes.ToListAsync());
+            }
+            return View(await dishes.ToListAsync());
         }
         public async Task<IActionResult> Details(int? id)
         {
