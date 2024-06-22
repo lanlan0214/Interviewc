eda49a899a652878247e6c0c254c6172b9cc788c
Frank <franklp5010606@gmail.com>
2024-06-22 09:38:28 +0800

    202406220938-Model-add

---
+++ Interview/Models/Dish.cs
@@ -0,0 +1,12 @@
+﻿namespace Interview.Models
+{
+    public class Dish
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ImageUrl { get; set; }
+        public int Price { get; set; }
+
+        public List<DishIngredient>? DishIngredients { get; set; }
+    }
+}
